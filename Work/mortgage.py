40f54e550155ed233a096288e714c0abf4f2b3d2
Milton Krauss <miltonkrauss@gmail.com>
2023-07-19 19:31:36 -0400

    completed CH 1 exercises

--- Work/mortgage.py
+++ Work/mortgage.py
@@ -7,16 +7,24 @@ interest_rate = 0.05
 monthly_payment = 2684.11
 total_paid = 0.0
 month_number = 0
-
-while month_number < 12:
-    principal = principal * (1+interest_rate/12) - (monthly_payment + 1000)
-    total_paid = total_paid + (monthly_payment + 1000)
-    month_number = month_number + 1
+extra_payment_start_month = 61
+extra_payment_end_month = 108
+extra_payment = 1000
 
 while principal > 0:
-    principal = principal * (1+interest_rate/12) - monthly_payment
-    total_paid = total_paid + monthly_payment
+    
+    while month_number >= extra_payment_start_month and month_number <= extra_payment_end_month:
+        principal = round(principal * (1+interest_rate/12) - (monthly_payment + extra_payment), 2)
+        total_paid = round(total_paid + (monthly_payment + extra_payment), 2)
+        month_number = month_number + 1
+        print(month_number, total_paid, principal)
+    principal = round(principal * (1+interest_rate/12) - monthly_payment, 2)
+    total_paid = round(total_paid + monthly_payment, 2)
     month_number = month_number + 1
+    print(month_number, total_paid, principal)
+
+    if principal < monthly_payment:
+        monthly_payment = principal * (1+interest_rate/12)
 
 print(f'Total paid: {total_paid:.2f}!')
 print('Total number of months:', month_number)
