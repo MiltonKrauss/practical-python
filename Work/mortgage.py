5111d763579e331d2fdb6ccbd36fa58967751bf9
Milton Krauss <miltonkrauss@gmail.com>
2023-07-19 17:02:39 -0400

    completed mortgage.py

--- Work/mortgage.py
+++ Work/mortgage.py
@@ -1,3 +1,14 @@
 # mortgage.py
 #
 # Exercise 1.7
+
+principal = 500000.0
+interest_rate = 0.05
+monthly_payment = 2684.11
+total_paid = 0.0
+
+while principal > 0:
+    principal = principal * (1+interest_rate/12) - monthly_payment
+    total_paid = total_paid + monthly_payment
+
+print('Total paid:', round(total_paid, ndigits=2))
